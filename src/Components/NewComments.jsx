9802f1391762e34ee25dcf9a11af4991ed212b51
fera1991 <00120520@uca.edu.sv>
2021-11-29 12:40:53 -0600

    prueba 4

--- src/Components/NewComments.jsx
+++ src/Components/NewComments.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShare } from '@fortawesome/free-solid-svg-icons';
-import { useState } from "react/cjs/react.development";
+import {useState} from "react";
 import { useAPIContext } from "../Context/Context";
 import swal from 'sweetalert';
 
